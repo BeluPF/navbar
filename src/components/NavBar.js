486199437707c940d613651115da589c83e886a1
Belen Pizarro <belupizarro@hotmail.com>
2021-11-16 15:11:47 -0300

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <nav>
+            <ul className="ul1">
+                <li className="li1"> COMERCIOLIBRE </li>
+                <li className="li1">Home</li>
+                <li className="li1">Nosotros</li>
+                <li className="li1">Soporte</li>
+                <li className="li1">Contactenos</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
