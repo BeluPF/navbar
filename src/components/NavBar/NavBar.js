92f2ba201b980c7456d21191d93e7af8c14d7804
Belen Pizarro <belupizarro@hotmail.com>
2021-12-16 20:11:29 -0300

    ccs

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -8,14 +8,16 @@ function NavBar() {
     let navigate = useNavigate();
     return (
         <nav className="nav">
-            <img src={logomarca} alt='logo marca' onClick={ () => navigate("/")}/>
+            
             <ul className="ul1">
-                
+            <img src={logomarca}  alt='logo marca' onClick={ () => navigate("/")}/>
+                <li className="li2">
+                <Icon name='shopping cart' className="shoppingCart" onClick={ () => navigate ("/cart")}></Icon>
+                </li>
                 <li className="li1" onClick={ () => navigate ("/impares")}>Impares</li>
                 <li className="li1" onClick={ () => navigate ("/pares")}>Pares</li>
-                
             </ul>
-            <Icon name='shopping cart' className="shoppingCart" onClick={ () => navigate ("/cart")}></Icon>
+            
         </nav>
     );
 }
